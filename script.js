d70ad74dacf4479bcdb6e952151678a79609456e
palpitate013 <github.palpitate013@passmail.net>
2025-05-30 16:58:31 -0400

    Add tracing feature and removed hover from css

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-for (i = 0; i < 256; i++){
-    let main = document.createElement("div");
-    main.className = "grid";
-    document.getElementById("main").appendChild(main);
+for (let i = 0; i < 256; i++) {
+  let main = document.createElement("div");
+  main.className = "grid" + i;
+  main.onmouseover = function () {
+    mouseOver(i);
+  };
+  main.onmouseout = function () {
+    mouseOut(i);
+  };
+  document.getElementById("main").appendChild(main);
 }
+
+function mouseOver(gridNumber) {
+  document.querySelector(".grid" + gridNumber).style.backgroundColor = "red";
+}
+
+function mouseOut() {}
