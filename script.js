e5075aa18d8a669f9767faf79143442e1b414295
palpitate013 <github.palpitate013@passmail.net>
2025-05-30 17:21:05 -0400

    Added reset button

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ for (let i = 0; i < 256; i++) {
   document.getElementById("main").appendChild(main);
 }
 
+let button = document.querySelector(".reset");
+
+button.addEventListener("click", function () {
+  for (i = 0; i < 256; i++) {
+    document.querySelector(".grid" + i).style.backgroundColor = "#282828";
+  }
+});
+
 function mouseOver(gridNumber) {
   document.querySelector(".grid" + gridNumber).style.backgroundColor = "red";
 }
